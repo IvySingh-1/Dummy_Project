d18cc9620a0aabc0222fb797b85640a6c98f92f4
Ivy Singh <ivysingh99@gmail.com>
2024-12-24 01:26:27 +0530

    updated navbar

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,9 +7,27 @@ import "./header.css"
 const Header = () => {
   return (
    <div>
+
+
       <Navbar expand="lg" className="bg-body-tertiary">
-      <Container className='tabs'>
+      <Container>
+        <img
+              alt=""
+              src="../../../src/assets/santa.png"
+              width="60"
+              height="60"
+              className="d-inline-block align-top"
+            />
         <Navbar.Brand className='Brand' href="#home">Expense Tracker</Navbar.Brand>
+
+        <img
+              alt=""
+              src="../../../src/assets/money-pot.png"
+              width="50"
+              height="60"
+              className="d-inline-block align-top"
+            />
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
