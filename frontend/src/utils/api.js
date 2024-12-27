32cd6c336fc012879d3c5f0dbe79492c5dd3ab65
Akshaansh Srivastava <48426245+Akshaansh1@users.noreply.github.com>
2024-12-27 14:30:55 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://expense-tracker-backend-h1vd.onrender.com/",
   withCredentials: true,
 });
 
